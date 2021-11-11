ba4ea1f2c0703963d78c94d459d028b9f07ec1d6
harini-spec <ponshriharini@gmail.com>
2021-11-11 22:01:17 +0530

    index template added

--- app.py
+++ app.py
@@ -15,9 +15,15 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
 
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def home_post():
+
+    link = request.form.get('input')
     return render_template('index.html')
 
 def startpy():
@@ -25,5 +31,5 @@ def startpy():
     app.run(debug=True)
 
 if __name__ == '__main__':
-    
+
     startpy()
